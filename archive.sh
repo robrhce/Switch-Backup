7d6097445d9bf09cb1cba10697e9963e4de9f235
Rob H <rob@rhce.com.au>
2025-05-01 16:35:20 +1000

    fixing archive.sh

--- archive.sh
+++ archive.sh
@@ -15,6 +15,6 @@ mkdir -p "$ARCHIVE_DIR"
 for f in "$LOCAL_DIR"/*; do
     [ -f "$f" ] || continue
     fname=$(basename "$f")
-    smbclient "\"$SMB_SHARE\"" -U "\"${SMB_USER}\"" --passowrd "\"${SMB_PASS}\"" -c "put \"$f\" \"$fname\" " && \
+    smbclient "$SMB_SHARE" -U "${SMB_USER}" --passowrd "${SMB_PASS}" -c "put \"$f\" \"$fname\" " && \
     mv "$f" "$ARCHIVE_DIR/"
 done
