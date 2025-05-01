75df9cc614b85b98abb79dfc954e92fe38fbc419
Rob H <rob@rhce.com.au>
2025-05-01 16:43:08 +1000

    fixed typo

--- archive.sh
+++ archive.sh
@@ -17,7 +17,7 @@ mkdir -p "$ARCHIVE_DIR"
 for f in "$LOCAL_DIR"/*; do
     [ -f "$f" ] || continue
     fname=$(basename "$f")
-    smbclient "$SMB_SHARE" -U "${SMB_USER}" --passowrd "${SMB_PASS}" -c "put \"$f\" \"$fname\" " && \
+    smbclient "$SMB_SHARE" -U "${SMB_USER}" --password "${SMB_PASS}" -c "put \"$f\" \"$fname\" " && \
     mv "$f" "$ARCHIVE_DIR/"
 done
 
